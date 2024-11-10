2c7e7b56204618aca7a9896d486c6782560e382c
xechnax <rsortilla@gmail.com>
2024-11-10 20:22:05 +0800

    Update header.php

--- header.php
+++ header.php
@@ -11,6 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!-- https://selftawt.com -->
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
