bea4ccbb594a15b7fb808581707366cf59c42cc4
xechnax <rsortilla@gmail.com>
2024-11-11 19:28:14 +0800

    Update header.php

--- header.php
+++ header.php
@@ -11,12 +11,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<!-- https://selftawt.com -->
+<!--
+*
+* This theme available for FREE. Open for feedback and feature requests.
+*
+* https://github.com/selftawt/blaggins-hybrid
+*
+* Author: Rey Sanchez
+*
+* https://selftawt.com
+*
+-->
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div class="site-container">
 <a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to main content', 'blaggins' ); ?></a>
 <?php get_template_part( 'template-parts/header/header-site' ) ?>
 
