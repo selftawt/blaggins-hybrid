14a71c3c2f6a1e7f121441ce6664e646e5f1ac11
xechnax <rsortilla@gmail.com>
2024-11-15 13:22:32 +0800

    Update header-logo.php

--- template-parts/header/header-logo.php
+++ template-parts/header/header-logo.php
@@ -1,15 +1,12 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-
 $site_title   = get_bloginfo( 'name' );
 $site_tagline = get_bloginfo( 'description' );
-?>
 
-<?php if ( has_custom_logo() ) : ?>
-	<?php the_custom_logo(); ?>
-<?php else: ?>
+if ( has_custom_logo() ) :
+	the_custom_logo();
+else :
+	?>
 	<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php esc_html_e( $site_title, 'blaggins' ) ?></a></p>
-<?php endif; ?>
+	<?php
+endif;
