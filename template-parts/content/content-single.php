4fe0b17235b35f54e5f51c80bf2ea0890d742a26
xechnax <rsortilla@gmail.com>
2024-11-15 10:06:32 +0800

    Update content-single.php

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -1,12 +1,13 @@
-<article id="post-<?php the_ID(); ?>" class="<?php post_class() ?>">
-	<header class="entry-header"><?php
-	if ( is_single() ) {
-		the_title( '<h1 class="entry-title">', '</h1>' );
-	} else {
-		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-	}
-	?></header><!-- .entry-header -->
-	<?php
-	the_content();
-	?>
-</article><!-- #post-<?php the_ID(); ?> -->
+<article <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( is_single() ) : ?>
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php else : ?>
+			<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+		<?php endif; ?>
+	</header>
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+</article>
+
