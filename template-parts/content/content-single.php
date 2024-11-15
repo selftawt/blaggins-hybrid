1a9594c3a49651e5e7aa42e5f97d0fb8e5fa96f5
xechnax <rsortilla@gmail.com>
2024-11-15 13:22:54 +0800

    Update content-single.php

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -1,13 +1,27 @@
+<?php
+/**
+ * Blaggins Hybrid
+ *
+ * @package Blaggins\Templates
+ * @author	Rey Sanchez
+ * @license	GPL-3.0
+ * @link	https://github.com/selftawt/blaggins-hybrid
+ */
+?>
+
 <article <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php if ( is_single() ) : ?>
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php else : ?>
-			<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-		<?php endif; ?>
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+		?>
 	</header>
+
 	<div class="entry-content">
 		<?php the_content(); ?>
-	</div>
+	</div><!-- .entry-content -->
 </article>
 
