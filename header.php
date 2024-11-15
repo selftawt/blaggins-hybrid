25be90aa95ad02ae430826dde680058cc4914f07
xechnax <rsortilla@gmail.com>
2024-11-15 10:06:11 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Blaggins;
-
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
@@ -12,20 +10,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <!--
-*
-* This theme available for FREE. Open for feedback and feature requests.
-*
-* https://github.com/selftawt/blaggins-hybrid
-*
-* Author: Rey Sanchez
-*
-* https://selftawt.com
-*
+                             –▀▀▀▀▀ ▀▀––   ▀▀
+ ▄██████ ▄███████▄     ▄█████████         ▄██████▄     ░
+█▀  ▀█████▀     ▀██  ░████▀▀▀▀██▀ ░    ████▀    ▀████  ░
+█    █████        ██ ░▓▓█▀ ░   ▀      █▓▓█ ░     ░█▓▓█ ░
+ ▀   █▓▓█        ▄░░─░▀▒▒▒▒▒▒▒█▄──░  █▒▒█ ░ ────   █▒▒█░
+ . ┌─█▓▓█        ▀   ░  ▀░░░░░░░█ ░  █░░█ ░        █░░█░
+   : █░░█    ..      ░     .  █..█ ░  █..█ ░      █..█ ░
+   . █..█            ░  ▄▀     █  █ ░ ░█  █▄    ▄█  █  ░
+   | █  █   ──────   ░ ██──────████ ░   ▀▀▄██████▄▀▀
+   └ ▀███            ░ ███▄  ▄████ ░   ░░░░      ░░░░░░░░
+      ░░░            ░  ▀███████▀          .
+
+   ┌──────▀▀▀▀ ▀▀──────── ██ ───────────▀▀▄▄▄  ▄▄▄──────┐
+   : ░                    ▀█▄    ▄             ▀▀       ░ :
 -->
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<div class="container__body-inner">
 <?php wp_body_open(); ?>
-<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to main content', 'blaggins' ); ?></a>
-<?php get_template_part( 'template-parts/header/header-site' ) ?>
+<a id="skipnav" class="screen-reader-text" href="#main"><?php esc_html_e( 'Skip to main content', 'blaggins' ); ?></a>
+<?php get_template_part( 'template-parts/header/header-site' ); ?>
+<main id="main">
+
 
