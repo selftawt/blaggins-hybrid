b094e90fb077e0da417954681fd91cde56d9f98e
xechnax <rsortilla@gmail.com>
2024-11-15 10:06:57 +0800

    Update header-site.php

--- template-parts/header/header-site.php
+++ template-parts/header/header-site.php
@@ -1,12 +1,4 @@
-<?php
-/**
- * Displays the site header.
- *
- */
-?>
-<header id="masthead" class="site-header" role="banner">
-
-	<?php get_template_part( 'template-parts/header/header-logo' ); ?>
-	<?php get_template_part( 'template-parts/header/header-menu' ); ?>
-
-</header><!-- #masthead -->
+<header id="header" itemscope itemtype="https://schema.org/WPHeader">
+	<div class="site-header__logo"><?php get_template_part( 'template-parts/header/header-logo' ); ?></div>
+	<div class="site-header__menu"><?php get_template_part( 'template-parts/header/header-menu' ); ?></div>
+</header>
