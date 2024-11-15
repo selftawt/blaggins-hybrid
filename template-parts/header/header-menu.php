b0ada3cb4a1e58e4e41565d74ec6affc4e5499f7
xechnax <rsortilla@gmail.com>
2024-11-15 10:06:53 +0800

    Update header-menu.php

--- template-parts/header/header-menu.php
+++ template-parts/header/header-menu.php
@@ -1,16 +1,12 @@
-<?php
-if ( has_nav_menu( 'menu-header' ) ) { ?>
-	<nav id="primary-navigation" class="menu-header" role="navigation" aria-label="<?php esc_attr_e( 'Header menu', 'blaggins' )?>" itemscope itemtype="https://schema.org/SiteNavigationElement">
+<?php if ( has_nav_menu( 'menu-header' ) ) : ?>
+	<nav id="header-nav" class="header__menu" aria-label="<?php esc_attr_e( 'Header menu', 'blaggins' )?>" itemscope itemtype="https://schema.org/SiteNavigationElement">
 	<?php
-	wp_nav_menu(
-		[
-			'theme-location'	=> 'main_nav',
-			'menu_class'		=> 'menu-header',
-			'items_wrap'		=> '<ul id="%1$s" class="%2$s">%3$s</ul>',
-		]
-	);
+	wp_nav_menu( [
+		'theme-location'	=> 'menu-header',
+		'menu_class'		=> 'header__menu',
+		'items_wrap'		=> '<ul id="%1$s" class="%2$s">%3$s</ul>',
+		'container'			=> '__return_false',
+	] );
 	?>
-	</nav><!-- #primary-navigation -->
-<?php
-}
-?>
+	</nav><!-- #header-nav -->
+<?php endif; ?>
